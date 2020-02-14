1db34d87323ad6a99ef6ffdbd9ff9200809c422a
Matthew Besedick <mtbesedick@gmail.com>
2020-02-14 11:37:30 -0600

    mapbox-gl api displaying on page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWJlc2VkaWNrIiwiYSI6ImNrNmxsMmhqcDBjenIzbHBkY2ExZDE3YjkifQ.xe3WJ0yxknKTi26HiAGr3g';
+
+const map = new mapboxgl.Map({
+	container: 'map',
+	center: [ -74.009, 40.705 ], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+	zoom: 12, // starting zoom
+	style: 'mapbox://styles/mapbox/streets-v10' // mapbox has lots of different map styles available.
+});
+
 console.log('testing');
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
-
 module.exports = {
 	mode: 'development',
 	entry: './src/index.js',
 	output: {
-		path: path.resolve(__dirname, 'public'),
+		path: __dirname + '/public',
 		filename: 'bundle.js'
 	}
 };
